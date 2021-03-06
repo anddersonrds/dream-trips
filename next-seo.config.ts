965cdc8bae3e2d53012b199ed854ec2c7dd81137
Anderson Rodrigues <anddersonrds@gmail.com>
2021-03-06 04:15:20 -0300

    Add Config Next-SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'http://dream-trips-anddersonrds.vercel.app',
+    site_name: 'Dream Trips'
+  },
+  twitter: {
+    handle: '@anddersonrds',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
